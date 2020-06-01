cc6c74900cff406b121d970f144c49621a49948f
irangareddy <itsmerr17@gmail.com>
2020-06-01 19:41:19 +0530

    Center and Text Widget

---
+++ HelloFlutter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// The main() is the starting point for all our Flutter Apps.
+void main() {
+  runApp(
+    // Material App confrims Material Design Style Created by Google
+    MaterialApp(
+      // Center widget helps to "Center" all the elements in it.
+      home: Center(
+        // Text widget helps to display the text.
+        child: Text("Hello  Flutter"),
+      ),
+    ),
+  );
+}
