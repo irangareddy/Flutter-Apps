fc9384623169e945bf44f4ef447791c423a69cfa
irangareddy <itsmerr17@gmail.com>
2020-06-16 19:20:39 +0530

    Optimized

--- Quiz/lib/main.dart
+++ Quiz/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:Quiz/quiz_brain.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
-import 'question.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quiz());
 
@@ -31,34 +35,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKepper = [];
 
-  List<Question> questionBank = [
-    Question(q: "A slug's blood is green.", a: true),
-    Question(
-        q: "Approximately one quarter of human bones are in the feet.",
-        a: true),
-    Question(
-        q: "The total surface area of two human lungs is approximately 70 square metres.",
-        a: true),
-    Question(
-        q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.",
-        a: true),
-    Question(
-        q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.",
-        a: false),
-    Question(q: "It is illegal to pee in the Ocean in Portugal.", a: true),
-    Question(q: "You can lead a cow down stairs but not up stairs.", a: false),
-    Question(q: "Google was originally called 'Backrub'.", a: true),
-    Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: true),
-    Question(
-        q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.",
-        a: false),
-    Question(
-        q: "No piece of square dry paper can be folded in half more than 7 times.",
-        a: false),
-    Question(
-        q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.",
-        a: true)
-  ];
+
 
   var questionNo = 0;
   int totalQuestions = 12;
@@ -125,7 +102,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNo].questionText,
+                quizBrain.getQuestionText(questionNo),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -154,7 +131,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  bool correctAnswer = questionBank[questionNo].questionAnswer;
+                  bool correctAnswer = quizBrain.getQuestionAnswer(questionNo);
 
                   if (correctAnswer == true) {
                     score += 1;
@@ -202,10 +179,13 @@ class _QuizPageState extends State<QuizPage> {
                   color: Colors.white,
                 ),
               ),
-              onPressed: () {
+              onPressed: (
+
+              ) {
+
 
                 setState(() {
-                  bool correctAnswer = questionBank[questionNo].questionAnswer;
+                  bool correctAnswer = quizBrain.getQuestionAnswer(questionNo);
 
                   if (correctAnswer == false) {
                     score += 1;
@@ -247,38 +227,11 @@ class _QuizPageState extends State<QuizPage> {
   void updateScore(int score,int highScore) {
     if (score > highScore) {
       highScore = score;
-      message(context, 'Congrats 🎉', 'You made a HighScore $highScore');
+      Alert(context: context, title: "Congratulations", desc: 'Hey you made highScore ever $highScore').show();
     } else {
       highScore = highScore;
-      message(context, 'Try Hard', 'Butter luck next time buddy');
+      Alert(context: context, title: 'Your Score', desc: '$score').show();
     }
   }
 
-  void message(BuildContext context, String title, String detailMessage) {
-    var alertDialog = CupertinoAlertDialog(
-      title: Text(
-        '$title',
-        style: TextStyle(
-          fontSize: 34.0,
-          fontWeight: FontWeight.w800,
-          color: Colors.pink,
-        ),
-      ),
-      content: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          '$detailMessage',
-          style: TextStyle(
-            fontSize: 24.0,
-          ),
-        ),
-      ),
-    );
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alertDialog;
-        });
-  }
 }
